41f102e60a747ea4d836762c3ea39905d0520fc9
olavkm <olav.k.m@gmail.com>
2015-03-23 14:38:51 +0100

    Update Client.py

--- Client.py
+++ Client.py
@@ -1,4 +1,6 @@
 # -*- coding: utf-8 -*-
+from MessageReceiver import MessageReceiver
+import json
 import socket
 
 class Client:
@@ -16,22 +18,81 @@ class Client:
         self.run()
 
         # TODO: Finish init process with necessary code
-
+        server_thread = MessageReceiver(client, self.connection)
+        server_thread.start() 
+        
+        while True:
+            data = raw_input('>> ')
+            self.send_payload(data)
+        
     def run(self):
         # Initiate the connection to the server
         self.connection.connect((self.host, self.server_port))
 
     def disconnect(self):
         # TODO: Handle disconnection
-        pass
+        self.connection.close()
 
     def receive_message(self, message):
         # TODO: Handle incoming message
-        pass
+        data = json.loads(message)
+
+        if data.get('response') == 'error':
+            # print error message
+            print data.get('content')
+            
+        elif data.get('response') == 'info':
+            if data.get('content') == 'logout':
+                print 'Goodbye ' + data.get('sender') + '!'
+                self.disconnect()
+            else:
+                print data.get('content')
+
+        elif data.get('response') == 'history':
+            print 'Welcome ' + data.get('sender') + '!'
+            history = data.get('content') # list of JSON responses
+            # print history
+            
+        elif data.get('response') == 'message':
+            # print message
+            print print_formatted(data.get('sender'),data.get('timestamp'),data.get('content'))
+        
 
     def send_payload(self, data):
-        # TODO: Handle sending of a payload
-        pass
+        # WIP
+        if data.startswith("login"):
+            username = data.lstrip('login ')
+            data = msg_format('login', username)
+            self.connection.sendall(json.dumps(data))
+            
+        elif data.startswith("logout"):
+            data = msg_format('logout', None)
+            self.connection.sendall(json.dumps(data))
+            
+        elif data.startswith("msg"):
+            message = data.lstrip('msg ')
+            data = msg_format('msg', message)
+            self.connection.sendall(json.dumps(data))
+            
+        elif data.startswith("help"):
+            data = msg_format('help', None)
+            self.connection.sendall(json.dumps(data))
+            
+        elif data.startswith("names"):
+            data = msg_format('names', None)
+            self.connection.sendall(json.dumps(data))
+            
+        else:
+            print data.split()[0] + ' is not a valid command!'
+        
+
+
+    def msg_format(request, content):                                                 
+        # Wait for data from the client
+        return {'request': request, 'content': content}
+
+    def print_formatted(sender,timestamp,message):
+        return sender + ' said @ ' + timestamp + ': ' + message
 
 
 if __name__ == '__main__':
@@ -41,4 +102,5 @@ if __name__ == '__main__':
 
     No alterations is necessary
     """
+    
     client = Client('localhost', 9998)
